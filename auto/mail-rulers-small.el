68b5411efadabfdc42e7fd08bfddec11e45b3682
Ricardo Hernandez M <hernanrr@email.sc.edu>
2015-08-23 19:19:28 -0400

    .gitignore shoudl now be working.

--- auto/mail-rulers-small.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook "mail-rulers-small"
- (lambda ()
-    (TeX-run-style-hooks
-     "graphicx"
-     "latex2e"
-     "standalone10"
-     "standalone")))
-
--- auto/test.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "test"
- (lambda ()
-    (TeX-run-style-hooks
-     "calc"
-     "tikz"
-     "latex2e"
-     "art10"
-     "article")))
-
